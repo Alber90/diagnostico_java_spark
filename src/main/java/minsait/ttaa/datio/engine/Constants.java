2d43db2f6267c9fbe02e6028429f46901e842039
Alber90 <89954250+Alber90@users.noreply.github.com>
2021-09-02 18:56:39 -0500

    Create Constants.java

---
+++ src/main/java/minsait/ttaa/datio/engine/Constants.java
@@ -0,0 +1,48 @@
+package minsait.ttaa.datio.engine;
+
+public final class Constants {
+	public static final String AGE_OPTION_A="**A**";
+	public static final String AGE_OPTION_B="**B**";
+	public static final String AGE_OPTION_C="**C**";
+	public static final String AGE_OPTION_D="**D**";
+	
+	
+
+}
+
+package minsait.ttaa.datio.engine;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties {
+	private String INPUT_PATH_CSV;
+	private String OUTPUT_PATH;
+	private String OUTPUT_PARQUET;
+	  ReadProperties() {
+  	  try {
+  		     Properties propiedades = new Properties();
+  		     propiedades
+  		       .load(new FileInputStream("src/test/resources/data/project.properties"));
+  		     INPUT_PATH_CSV= propiedades.getProperty("read.input.path.csv");
+  		     OUTPUT_PATH = propiedades.getProperty("read.output.path");
+  		     OUTPUT_PARQUET = propiedades.getProperty("read.output.parquet");
+  		    } catch (FileNotFoundException e) {
+  		     System.out.println("Error, El archivo no exite");
+  		    } catch (IOException e) {
+  		     System.out.println("Error, No se puede leer el archivo");
+  		    }
+  		   }
+	public String getINPUT_PATH_CSV() {
+		return INPUT_PATH_CSV;
+	}
+	public String getOUTPUT_PATH() {
+		return OUTPUT_PATH;
+	}
+	public String getOUTPUT_PARQUET() {
+		return OUTPUT_PARQUET;
+	}
+
+}

a758d664e6e40d0edb48a33b4d54abfbb19f9795
Alber90 <89954250+Alber90@users.noreply.github.com>
2021-09-02 18:50:43 -0500

    Update Common.java

--- src/main/java/minsait/ttaa/datio/common/Common.java
+++ src/main/java/minsait/ttaa/datio/common/Common.java
@@ -1,11 +1,15 @@
 package minsait.ttaa.datio.common;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public final class Common {
 
     public static final String SPARK_MODE = "local[*]";
     public static final String HEADER = "header";
     public static final String INFER_SCHEMA = "inferSchema";
-    public static final String INPUT_PATH = "src/test/resources/data/players_21.csv";
-    public static final String OUTPUT_PATH = "src/test/resources/data/output";
-
+        		  
 }
+
